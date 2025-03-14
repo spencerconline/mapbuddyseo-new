ea56ff6c8dd1097c53b2524a6df217232beeea2c
spencerconline <claeyssc@gmail.com>
2025-03-13 23:47:23 -0700

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-green-400 p-4">
+      <h1 className="text-4xl">MapBuddy SEO</h1>
+      <p>Territory map loading...</p>
+    </div>
+  );
+}
